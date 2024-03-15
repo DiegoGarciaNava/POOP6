e8264c75bee9c65d34e4f0bd363f92f9512c933b
DiegoGarciaNava <90359547+DiegoGarciaNava@users.noreply.github.com>
2024-03-15 08:41:03 -0600

    Creación de practica 6

---
+++ mx/unam/fi/poo/POOP6.java
@@ -0,0 +1,12 @@
+package mx.unam.fi.poo;
+
+/**
+ * Programa que muestra un hola mundo
+ */
+
+public class POOP6{
+
+    public static void main(String[] args){
+        System.out.println("Hola mundo");
+    }
+}
